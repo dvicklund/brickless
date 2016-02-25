020b7c54f97457a104ca3c9cc02434413d87fd9b
tom conley <tom@toms-MacBook.local>
2016-02-24 21:44:34 -0800

    Connect image upload controller with the profile.html

--- app/js/directives/imageupload.js
+++ app/js/directives/imageupload.js
@@ -1,10 +1,32 @@
 module.exports = function(app) {
   app.directive('imageupload', function() {
+		
+		var controller = ['$scope', 'Upload', '$timeout', function ($scope, Upload, $timeout) {
+		    $scope.uploadPic = function(file) {
+		      file.upload = Upload.upload({
+		        url: '/img/upload',
+		        data: {file: file, username: $scope.username},
+		      });
+
+		      file.upload.then(function (response) {
+		        $timeout(function () {
+		          file.result = response.data;
+		        });
+		      }, function (response) {
+		        if (response.status > 0)
+		          $scope.errorMsg = response.status + ': ' + response.data;
+		      }, function (evt) {
+		        // Math.min is to fix IE which reports 200% sometimes
+		        file.progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
+		      });
+		    }
+		  }];
+
     return {
-      restrict: 'C',
+      restrict: 'E', // was C
       replace: true,
-      controller: 'js/controllers/imageuploadCtrl.js',
+      controller: controller,
       templateUrl: 'html/imageupload.html'
-    }
-  })
-}
+    };
+  });
+};
